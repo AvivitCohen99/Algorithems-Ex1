d4488dfe77678d162eac44c022623cd248b61240
AvivitCohen99 <avivit.cohen99@gmail.com>
2023-04-04 14:49:45 +0300

    creating first files

---
+++ Graph.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITHMS_EX1_GRAPH_H
+#define ALGORITHMS_EX1_GRAPH_H
+
+
+class Graph {
+
+
+
+};
+
+
+#endif //ALGORITHMS_EX1_GRAPH_H
---
+++ UI.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHMS_EX1_UI_H
+#define ALGORITHMS_EX1_UI_H
+
+class UI{
+
+};
+
+#endif //ALGORITHMS_EX1_UI_H
